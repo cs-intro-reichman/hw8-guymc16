e0590ae982c998311c982f89e8ff10c58e1e6b93
guy michaeli <guy.michaeli@post.runi.ac.il>
2025-01-08 01:29:11 +0200

    Update Network.java

--- Network.java
+++ Network.java
@@ -106,9 +106,9 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String ans = "Network:\n";
+        String ans = "Network:";
        for (int i = 0; i <userCount; i ++) {
-        ans += users[i] + "\n";
+        ans += "\n" + users[i];
        }
        return ans;
     }
